ccb14816acecee08f57ffa910001288a44fbbf48
Vangelis Tsiatouras <vangelis.tsiatouras@gmail.com>
2020-12-31 18:14:06 +0200

    [test]: mock database properly

--- app/tests/conftest.py
+++ app/tests/conftest.py
@@ -1,6 +1,7 @@
-from typing import Generator
-
+import mongomock
 import pytest
+
+from typing import Generator
 from fastapi.testclient import TestClient
 
 from app.main import app
@@ -10,3 +11,8 @@ from app.main import app
 def client() -> Generator:
     with TestClient(app) as c:
         yield c
+
+
+async def override_db_conn():
+    mock_client = mongomock.MongoClient()
+    return mock_client['incidents']
