7535a0a09d731a291a0099ada4808245a39559fc
Vangelis Tsiatouras <vangelis.tsiatouras@gmail.com>
2021-01-06 01:34:05 +0200

    [feat]: added endpoint to fetch total requests per type

--- app/api/v1/api.py
+++ app/api/v1/api.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
 
-from app.api.v1.endpoints import home
+from app.api.v1.endpoints import home, queries
 
 api_router = APIRouter()
 api_router.include_router(home.router, tags=["home"])
+api_router.include_router(queries.router, tags=["queries"])
