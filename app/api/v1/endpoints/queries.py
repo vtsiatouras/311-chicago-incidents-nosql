f683d521a5e4bd859df95e10aa1736f3954cf3bf
Vangelis Tsiatouras <vangelis.tsiatouras@gmail.com>
2021-01-07 01:50:05 +0200

    [fix]: added date validation at total_requests_per_type

--- app/api/v1/endpoints/queries.py
+++ app/api/v1/endpoints/queries.py
@@ -1,6 +1,5 @@
 from typing import Any, List
-from fastapi import APIRouter, Depends
-from fastapi.encoders import jsonable_encoder
+from fastapi import APIRouter, Depends, HTTPException
 from pymongo.database import Database
 from datetime import datetime
 
@@ -15,6 +14,13 @@ def total_requests_per_type(start_date: datetime, end_date: datetime, db: Databa
     """ Find the total requests per type that were created within a specified time range and sort
     them in a descending order.
     """
+    if end_date < start_date:
+        raise HTTPException(
+            status_code=422,
+            detail="'start_date' must be less than 'end_date'",
+            headers={"X-Error": "Validation Error"},
+        )
+
     cursor = db['incidents'].aggregate([
         {"$match": {"creation_date": {"$gte": start_date, "$lte": end_date}}},
         {"$project": {"_id": 1, "type_of_service_request": 1}},
