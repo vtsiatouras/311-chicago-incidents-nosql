61e309c1e1c4367b3f43e2794434b0453fe132a1
Vangelis Tsiatouras <vangelis.tsiatouras@gmail.com>
2021-01-08 21:50:07 +0200

    [perf]: added more indexes

--- app/import_scripts/create_indexes.py
+++ app/import_scripts/create_indexes.py
@@ -5,8 +5,9 @@ from app.db.db_connection import get_db
 
 def create_indexes():
     db = next(get_db())
-    # db['incidents'].create_index('type_of_service_request')
+    db['incidents'].create_index([('type_of_service_request', pymongo.ASCENDING)])
     db['incidents'].create_index([('creation_date', pymongo.ASCENDING)])
+    db['incidents'].create_index([('type_of_service_request', pymongo.ASCENDING), ('creation_date', pymongo.ASCENDING)])
 
 
 if __name__ == '__main__':
