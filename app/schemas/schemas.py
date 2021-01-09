493145af39eb2f4cec0f5156b5f1e886430d94c1
Vangelis Tsiatouras <vangelis.tsiatouras@gmail.com>
2021-01-09 02:40:37 +0200

    [style]: replace double quotes to single

--- app/schemas/schemas.py
+++ app/schemas/schemas.py
@@ -7,9 +7,9 @@ class TypeOfServiceRequest(str, Enum):
     graffiti = 'GRAFFITI'
     garbage = 'GARBAGE'
     pothole = 'POTHOLE'
-    rodent_baiting = "RODENT_BAITING"
+    rodent_baiting = 'RODENT_BAITING'
     sanitation_violation = 'SANITATION_VIOLATION'
-    street_one_light = "STREET_ONE_LIGHT"
-    street_all_lights = "STREET_ALL_LIGHTS"
-    tree_trim = "TREE_TRIM"
-    tree_debris = "TREE_DEBRIS"
+    street_one_light = 'STREET_ONE_LIGHT'
+    street_all_lights = 'STREET_ALL_LIGHTS'
+    tree_trim = 'TREE_TRIM'
+    tree_debris = 'TREE_DEBRIS'
