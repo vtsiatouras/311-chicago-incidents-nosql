4e4ad020e5df5edac7b47a0cba580b00e537c146
Vangelis Tsiatouras <vangelis.tsiatouras@gmail.com>
2021-01-10 04:25:36 +0200

    [fix]: possible CI fix #6

--- .github/workflows/init_mongo_user.js
+++ .github/workflows/init_mongo_user.js
@@ -5,7 +5,7 @@ db.createUser(
         roles:[
             {
                 role: "readWrite",
-                db:   "mydatabase"
+                db:   "chicago_incidents_test"
             }
         ]
     }
