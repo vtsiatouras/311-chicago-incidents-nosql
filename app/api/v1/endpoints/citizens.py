0cd1d92e38eb60e0f78c5882232fb0d772aad7d7
Vangelis Tsiatouras <vangelis.tsiatouras@gmail.com>
2021-01-15 01:31:41 +0200

    [fix]: fix some bugs at create-upvote endpoint

--- app/api/v1/endpoints/citizens.py
+++ app/api/v1/endpoints/citizens.py
@@ -12,7 +12,6 @@ from app.models.models import CitizenCreateVote, DocumentID
 router = APIRouter()
 
 
-# TODO TESTS
 @router.post('/create-upvote', response_model=DocumentID)
 def create_upvote(
         *,
@@ -40,14 +39,17 @@ def create_upvote(
     if citizen_db_data and citizen_db_data.get('_id', None) and incident.get('voted_by', None) \
             and citizen_db_data.get('_id') in incident.get('voted_by'):
         raise HTTPException(
-            status_code=201,
+            status_code=409,
             detail=f"You have already voted incident with id '{incident_id}'",
+            headers={'X-Error': 'Double Submit'},
         )
 
+    incident_ward = incident.get('ward', None)
+    citizen_wards = citizen_db_data.get('wards', None) if citizen_db_data else None
     update_dict = {
         '$addToSet': {
             'voted_incidents': incident_id,
-            'wards': incident.get('ward', None)
+            'wards': incident_ward
         },
         '$inc': {
             'total_votes': 1,
@@ -56,8 +58,7 @@ def create_upvote(
     }
 
     # If incident does not has ward or incident's ward already exists remove the update actions of the above dictionary
-    if not incident.get('ward', None) or citizen_db_data and citizen_db_data.get('_id', None) \
-            and incident['ward'] in citizen_db_data.get('wards', None):
+    if not incident_ward or (citizen_db_data and citizen_wards and incident_ward in citizen_wards):
         update_dict['$addToSet'].pop('wards')
         update_dict['$inc'].pop('total_wards')
 
